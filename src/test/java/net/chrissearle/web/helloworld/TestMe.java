555edbd632b9b1ed920449c0c392887939176afb
chrsearl <chris.searle@finn.no>
2014-01-20 10:46:45 +0100

    Initial

---
+++ src/test/java/net/chrissearle/web/helloworld/TestMe.java
@@ -0,0 +1,14 @@
+package net.chrissearle.web.helloworld;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestMe {
+
+    @Test
+    public void testAlwaysPass() {
+        assertTrue(true);
+    }
+
+}
